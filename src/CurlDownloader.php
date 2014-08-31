cf8f3486b3c3c4f8ba2c5fe1877af5a252a2cd29
Tomas Majer <tomasmajer@gmail.com>
2014-08-31 10:21:35 +0200

    Fixed psr error

--- src/CurlDownloader.php
+++ src/CurlDownloader.php
@@ -4,24 +4,24 @@ namespace Tomaj\RssDownloader;
 
 class CurlDownloader implements DownloaderInterface
 {
-	private $timeout;
+    private $timeout;
 
     public function __construct($timeout = 5)
     {
         if (!extension_loaded('curl')) {
-        	throw new \Exception("Need extension 'curl' for CurlDownloader");
+            throw new \Exception("Need extension 'curl' for CurlDownloader");
         }
         $this->timeout = $timeout;
     }
 
     public function fetch($url)
     {
-        $ch = curl_init(); 
-        curl_setopt($ch, CURLOPT_URL, $url); 
-        curl_setopt($ch, CURLOPT_HEADER, true); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout * 1000);
-        $content = curl_exec($ch); 
+        $content = curl_exec($ch);
         curl_close($ch);
         return $content;
     }
